44ce6ed48afc9ba0ac9e2c3fcd3be8c757132af4
Adrian Costin <adrian@adriancostin.ro>
2019-05-26 16:57:58 +0300

    sdf

--- letsencrypt-dns/run.sh
+++ letsencrypt-dns/run.sh
@@ -20,7 +20,7 @@ if [ ! -d "$CERT_DIR/live" ]; then
     done
 
     echo "$DOMAINS" > /data/domains.gen
-    certbot certonly --non-interactive --standalone --manual --email "$EMAIL" --agree-tos --config-dir "$CERT_DIR" --work-dir "$WORK_DIR" --preferred-challenges "dns" "${DOMAIN_ARR[@]}"
+    certbot certonly --non-interactive --manual --email "$EMAIL" --agree-tos --config-dir "$CERT_DIR" --work-dir "$WORK_DIR" --preferred-challenges "dns" "${DOMAIN_ARR[@]}"
 
 # Renew certs
 else
